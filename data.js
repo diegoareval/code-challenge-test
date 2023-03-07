c812cba4e70f874ea54a6fa35d2b6ee9d1627dd5
Diego <diego2000avelar@gmail.com>
2023-03-07 15:22:51 -0600

    chore: project base

---
+++ data.js
@@ -0,0 +1,33 @@
+
+let rockyObj = {
+    'Name': 'Rocky',
+    'Favorite Food': 'Sushi',
+    'Favorite Movie': 'Back to The Future',
+    'Status': 'Inactive'
+  }
+  let miroslavObj = {
+    'Name': 'Miroslav',
+    'Favorite Food': 'Sushi',
+    'Favorite Movie': 'American Psycho',
+    'Status': 'Active'
+  }
+  let donnyObj = {
+    'Name': 'Donny',
+    'Favorite Food': 'Singapore chow mei fun',
+    'Favorite Movie': 'The Princess Bride',
+    'Status': 'Inactive'
+  }
+  let mattObj = {
+    'Name': 'Matt',
+    'Favorite Food': 'Brisket Tacos',
+    'Favorite Movie': 'The Princess Bride',
+    'Status': 'Active'
+  }
+
+ const fullData = [rockyObj, miroslavObj, donnyObj, mattObj];
+
+
+ module.exports = {data: fullData}
+
+
+
---
+++ index.js
@@ -0,0 +1,62 @@
+// name: Diego Arevalo, march 7, 2023
+
+const { data } = require('./data')
+
+// class receive array of data to manipulate it
+class DataHandler {
+  /**
+   * Represents a array of person preferences.
+   * @constructor
+   * @param {array} data - it is an array of object.
+   */
+  constructor(data) {
+    this.data = data.map((item) => ({ ...item, date: new Date() }))
+  }
+
+  /**
+   * should filter a data according the specified status.
+   * @function
+   * @param {string} status - Status could be Active or Inactive.
+   * @returns(object[])
+   */
+  filteredData(status) {
+    return this.data.filter((item) => item['Status'] === status)
+  }
+
+  /**
+   * show the data, returning only the keys added in the whitelist array.
+   * @function
+   * @param {string[]} whiteList - they are all the keys we want to return once we filter.
+   * @param {string} status - Status could be Active or Inactive.
+   * @returns(object[])
+   */
+  show(whiteList, status) {
+    const filteredData = this.filteredData(status)
+    return filteredData.map((item) => {
+      return whiteList.map((list) => {
+        return {
+          [list]: item[list],
+        }
+      })
+    })
+  }
+
+  /**
+   * show default data.
+   * @function
+   * @returns(object[])
+   */
+  display() {
+    return this.data
+  }
+}
+
+// array of keys that should be returned in the dataHandler.show() method
+const whiteList = ['Name', 'date', 'Favorite Movie']
+
+const dataHandler = new DataHandler(data)
+
+console.log('......ataching date')
+console.log(dataHandler.display())
+console.log('......filtered by active')
+console.log(dataHandler.show(whiteList, 'Active'))
